0d3a3fd8703d3ac347d9d2a5186d1fd5bc5fcab0
Assaf <1214assaf@gmail.com>
2024-06-15 12:32:20 +0300

    feat: Improve SearchableList component search functionality
    The SearchableList component is updated to include improved search functionality. The handleChange function now uses a debounce technique to delay the search term update, resulting in smoother and more efficient searching.

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 export default function SearchableList({items, itemKeyFn ,children}){
+    const lastChange = useRef();
     const [searchTerm, setSearchTerm] = useState('');
 
     const searchResults =  items.filter(item => JSON.stringify(item).toLocaleLowerCase()
                                 .includes(searchTerm.toLocaleLowerCase()));
 
     function handleChange(event){
-        setSearchTerm(event.target.value);
+
+        if(lastChange.current){
+            clearTimeout(lastChange.current);
+        } 
+
+        lastChange.current = setTimeout(()=> {
+            lastChange.current = null;
+            setSearchTerm(event.target.value);
+        }, 500);
     }
     return <div className="searchable-list">
         <input type="search" placeholder="Search" onChange={handleChange} />
