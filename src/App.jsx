7fbaabe665b535cbf8d7a073b796f12fe759d21b
Assaf <1214assaf@gmail.com>
2024-06-15 11:35:38 +0300

    feat: Refactor Accordion component to include AccordionTitle and AccordionContent components

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ function App() {
     <section>
       <h2>Why work with us?</h2>
       <Accordion className="accordion">
-        <Accordion.Item id="experience" className="accordion-item" title="We got 20 years of experience">
+        <Accordion.Item >
+          <Accordion.Title className="accordion-item-title" id="experience">We got 20 years of experience</Accordion.Title>
+          <Accordion.Content className="accordion-item-content"  id="experience">
           <article>
             <p>You can&apos;t go wrong with us.</p>
           <p>We are in the business of planning highly individualized vacation trips for more than 20 years. </p></article>
+          </Accordion.Content>
         </Accordion.Item>
 
-        <Accordion.Item id="local-guides" className="accordion-item" title="We are working with local guides">
+        <Accordion.Item>
+          <Accordion.Title className="accordion-item-title" id="local-guides">We are working with local guides</Accordion.Title>
+          <Accordion.Content className="accordion-item-content" id="local-guides">
           <article>
             <p>We are not doing this along from our office.</p>
           <p>We work with local guides to ensure safe and pleasant vacation </p></article>
+          </Accordion.Content>
         </Accordion.Item>
       </Accordion>
     </section>
