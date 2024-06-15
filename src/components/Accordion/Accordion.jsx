6a00779020a83219230ab0e17d890ee5e77e4703
Assaf <1214assaf@gmail.com>
2024-06-15 11:13:55 +0300

    refactor Accordion component to use toggleItem function for state management

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -16,17 +16,14 @@ export function useAccordionContext(){
 
 export default function Accordion({children, className}){
     const [openItemId, setOpenItemId] = useState(null)
-    function openItem(id){
-        setOpenItemId(id)
-    }   
 
-    function closeItem(){
-        setOpenItemId(null)
+    function toggleItem(id){
+        setOpenItemId(prevId => prevId === id ? null : id)
     }
+
     const contextValue = {
         openItemId: openItemId,
-        openItem,
-        closeItem,
+        toggleItem,
     }
 
     return (<AccordionContext.Provider value={contextValue}><ul className={className}>
